abe4f463238bebbf66825d317bb8474a6b0a3896
aulialvianto <pitot.crush@gmail.com>
2021-11-02 12:02:35 +0700

    File DPT

--- sistem/upload_dpt.php
+++ sistem/upload_dpt.php
@@ -141,7 +141,27 @@ $no_urut= mysqli_real_escape_string($koneksi, $_POST['no_urut']);
               <div class="row">
                 <div class="col-lg-12">
                   <h2><i class="fa fa-user"> Upload DPT</i></h2>
-                   
+                  <form action="" method="post" enctype="multipart/form-data">
+                      <div class="form-group">
+                        <label>NIM</label>
+                        <input type="text" name="nim" required="required" class="form-control">
+                      </div>
+                      <div class="form-group">
+                        <label>Nama</label>
+                        <input type="text" name="nama_mhs" required="required" autocomplete="off" class="form-control">
+                      </div>
+                      <div class="form-group">
+                        <label>Falkultas</label>
+                        <input type="text" name="falkultas" required="required" class="form-control">
+                      </div>
+                      <div class="form-group">
+                        <label>Semester</label>
+                        <input type="text" name="semester" required="required" autocomplete="off" class="form-control">
+                      </div>
+                      <div class="form-group">
+                        <input type="submit" class="btn btn-success" name="simpan" value="Input" class="form-control">
+                      </div>
+                  </form>   
                 </div>
               </div>
 
