f184c3a1ede5e565548ea574c649dd0150e0a1c8
arof44 <arafahnur57@gmail.com>
2021-12-08 21:43:37 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,11 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","voting");
+$hostname = "localhost";
+$username = "root";
+$pass = "";
+$dbasename = "voting";
+
+$koneksi = mysqli_connect($hostname, $username, $pass, $dbasename);
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
